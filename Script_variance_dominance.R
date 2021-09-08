7ecb331571d19d5fb3f0790841d4bc9112b337d3
Josselin Clo <56598380+JosselinCLO@users.noreply.github.com>
2021-09-08 10:59:10 +0200

    Update Script_variance_dominance.R

--- Script_variance_dominance.R
+++ Script_variance_dominance.R
@@ -1,5 +1,7 @@
 Estimates = read.table("Estimation.csv", header = T, dec=",", sep=";")
 
+# Means
+
 ratio_VD = c(NULL)
 ratio_VDI = c(NULL)
 ratio_COVadi = c(NULL)
@@ -14,12 +16,35 @@ for(i in 1:nrow(Estimates)){
   
 }
 
-wilcox.test(ratio_VD)
-wilcox.test(ratio_VDI)
-wilcox.test(ratio_COVadi)
-wilcox.test(ratio_H)
-
 mean(ratio_VD)
 mean(ratio_VDI)
 mean(ratio_COVadi)
 mean(ratio_H)
+
+# Bootstrap 
+
+nb_rep = 100
+
+ratio_VD_boot = c(NULL)
+ratio_VDI_boot = c(NULL)
+ratio_COVadi_boot = c(NULL)
+ratio_H_boot = c(NULL)
+
+for(i in 1:nb_rep){
+  
+  sub_sample = sample(1:40, 40, replace = T)  
+  
+  for(j in 1:length(sub_sample)){
+    
+    sub_temp = Estimates[sub_sample[j],]
+    
+    if(j == 1){sub_fin = sub_temp}
+    else{sub_fin = rbind(sub_fin, sub_temp)}
+  }
+
+ratio_VD_boot[i] = mean(sub_fin$V_DO/(sub_fin$V_A+sub_fin$V_DO))
+ratio_VDI_boot[i] = mean(sub_fin$V_DI/(sub_fin$V_A+sub_fin$V_DI)) 
+ratio_COVadi_boot[i] = mean(abs(sub_fin$Cov_a_d)/(sub_fin$V_A+abs(sub_fin$Cov_a_d)))
+ratio_H_boot[i] = mean(sub_fin$H/(sub_fin$V_A+sub_fin$H))
+  
+}
